b73c1029f66cf02f4e9abba236dde1ac30d161c0
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-09-10 23:25:38 -0500

    RambleyPainter is now a ListenedPainter
    RambleyPainter now extends ListenedPainter, and the methods and fields that were shared with ListenedPainter have been removed from RambleyPainter.

--- src/raccoon/RambleyPainter.java
+++ src/raccoon/RambleyPainter.java
@@ -7,11 +7,9 @@ package raccoon;
 import geom.*;
 import java.awt.*;
 import java.awt.geom.*;
-import java.beans.*;
 import java.util.*;
 import java.util.function.DoubleUnaryOperator;
-import javax.swing.*;
-import javax.swing.event.*;
+import swing.ListenedPainter;
 
 /**
  * This is a Painter that paints Rambley the Raccoon from Indigo Park. 
@@ -23,7 +21,7 @@ import javax.swing.event.*;
  * @author Milo Steier
  * @author AnimalWave on Discord
  */
-public class RambleyPainter implements Painter<Component>{
+public class RambleyPainter extends ListenedPainter<Component>{
     /**
      * This is the main color for the background.
      */
@@ -817,21 +815,11 @@ public class RambleyPainter implements Painter<Component>{
     public static final String RAMBLEY_OPEN_MOUTH_HEIGHT_PROPERTY_CHANGED = 
             "RambleyOpenMouthHeightPropertyChanged";
     
-    /*                      Settings and Listeners
+    /*                          Settings
     
-    These are fields that relate to controlling the settings for the painter and 
-    for listening to this painter.
+    These are fields that relate to controlling the settings for the painter.
     */
     
-    /**
-     * This is an EventListenerList to store the listeners for this class.
-     */
-    protected EventListenerList listenerList = new EventListenerList();
-    /**
-     * This is the PropertyChangeSupport used to handle changes to the 
-     * properties of this painter.
-     */
-    private final PropertyChangeSupport changeSupport;
     /**
      * This stores the flags used to store the settings for this painter.
      */
@@ -1169,7 +1157,6 @@ public class RambleyPainter implements Painter<Component>{
         eyeRightX = eyeRightY = eyeLeftX = eyeLeftY = 0.0;
         mouthOpenWidth = 1.0;
         mouthOpenHeight = 0.0;
-        changeSupport = new PropertyChangeSupport(this);
     }
     /**
      * This constructs a {@code RambleyPainter} object with the given flags.
@@ -5882,6 +5869,7 @@ public class RambleyPainter implements Painter<Component>{
      * implementations.
      * @return A String representation of this {@code RambleyPainter}.
      */
+    @Override
     protected String paramString(){
         return "flags="+getFlags()+
                 ",dotSize="+getBackgroundDotSize()+
@@ -5892,230 +5880,6 @@ public class RambleyPainter implements Painter<Component>{
                 ",leftEye=("+getRambleyLeftEyeX()+","+getRambleyLeftEyeY()+")"+
                 ",mouthOpen="+getRambleyOpenMouthWidth()+"x"+
                     getRambleyOpenMouthHeight();
-    }
-    @Override
-    public String toString(){
-        return getClass().getName()+"["+paramString()+"]";
-    }
-    /**
-     * This returns an array of all the objects currently registered as 
-     * <code><em>Foo</em>Listener</code>s on this icon. 
-     * <code><em>Foo</em>Listener</code>s are registered via the 
-     * <code>add<em>Foo</em>Listener</code> method. <p>
-     * 
-     * The listener type can be specified using a class literal, such as 
-     * <code><em>Foo</em>Listener.class</code>. If no such listeners exist, then 
-     * an empty array will be returned.
-     * @param <T> The type of {@code EventListener} being requested.
-     * @param listenerType The type of listeners being requested. This should 
-     * be an interface that descends from {@code EventListener}.
-     * @return An array of the objects registered as the given listener type on 
-     * this icon, or an empty array if no such listeners have been added.
-     */
-    @SuppressWarnings("unchecked")
-    public <T extends EventListener> T[] getListeners(Class<T> listenerType){
-            // If we're getting the PropertyChangeListeners
-        if (listenerType == PropertyChangeListener.class)
-            return (T[])getPropertyChangeListeners();
-        else
-            return listenerList.getListeners(listenerType);
-    }
-    /**
-     * This adds a {@code PropertyChangeListener} to this painter. This listener 
-     * is registered for all bound properties of this painter. 
-     * @param l The listener to be added.
-     * @see #addPropertyChangeListener(String, PropertyChangeListener) 
-     * @see #removePropertyChangeListener(PropertyChangeListener) 
-     * @see #getPropertyChangeListeners() 
-     */
-    public void addPropertyChangeListener(PropertyChangeListener l){
-        changeSupport.addPropertyChangeListener(l);
-    }
-    /**
-     * This adds a {@code PropertyChangeListener} to this painter that listens for 
-     * a specific property.
-     * @param propertyName The name of the property to listen for.
-     * @param l The listener to be added.
-     * @see #addPropertyChangeListener(PropertyChangeListener) 
-     * @see #removePropertyChangeListener(String, PropertyChangeListener) 
-     * @see #getPropertyChangeListeners(String) 
-     */
-    public void addPropertyChangeListener(String propertyName, 
-            PropertyChangeListener l){
-        changeSupport.addPropertyChangeListener(propertyName, l);
-    }
-    /**
-     * This removes a {@code PropertyChangeListener} from this painter. This 
-     * method should be used to remove {@code PropertyChangeListener}s that were 
-     * registered for all bound properties of this painter. 
-     * @param l The listener to be removed.
-     * @see #addPropertyChangeListener(PropertyChangeListener) 
-     * @see #removePropertyChangeListener(String, PropertyChangeListener) 
-     * @see #getPropertyChangeListeners() 
-     */
-    public void removePropertyChangeListener(PropertyChangeListener l){
-        changeSupport.removePropertyChangeListener(l);
-    }
-    /**
-     * This removes a {@code PropertyChangeListener} to this painter that listens 
-     * for a specific property. This method should be used to remove {@code 
-     * PropertyChangeListener}s that were registered for a specific property
-     * @param propertyName The name of the property.
-     * @param l The listener to be removed.
-     * @see #removePropertyChangeListener(PropertyChangeListener)
-     * @see #addPropertyChangeListener(String, PropertyChangeListener) 
-     * @see #getPropertyChangeListeners(String) 
-     */
-    public void removePropertyChangeListener(String propertyName, 
-            PropertyChangeListener l){
-        changeSupport.removePropertyChangeListener(propertyName, l);
-    }
-    /**
-     * This returns an array of all {@code PropertyChangeListener}s that are 
-     * registered on this painter.
-     * @return An array of the {@code PropertyChangeListener}s that have been 
-     * added, or an empty array if no listeners have been added.
-     * @see #getPropertyChangeListeners(String) 
-     * @see #addPropertyChangeListener(PropertyChangeListener) 
-     * @see #removePropertyChangeListener(PropertyChangeListener) 
-     */
-    public PropertyChangeListener[] getPropertyChangeListeners(){
-        return changeSupport.getPropertyChangeListeners();
-    }
-    /**
-     * This returns an array of all {@code PropertyChangeListener}s that are 
-     * registered on this painter for a specific property.
-     * @param propertyName The name of the property.
-     * @return An array of the {@code PropertyChangeListener}s that have been 
-     * added for the specified property, or an empty array if no listeners have 
-     * been added or the specified property is null.
-     * @see #getPropertyChangeListeners() 
-     * @see #addPropertyChangeListener(String, PropertyChangeListener) 
-     * @see #removePropertyChangeListener(String, PropertyChangeListener) 
-     */
-    public PropertyChangeListener[] getPropertyChangeListeners(String propertyName){
-        return changeSupport.getPropertyChangeListeners(propertyName);
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name,
-     * old value, and new value. This method is for {@code Object} properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, Object oldValue, 
-            Object newValue){
-            // If the PropertyChangeSupport has been initialized
-        if (changeSupport != null)  
-            changeSupport.firePropertyChange(propertyName, oldValue, newValue);
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for {@code boolean} properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, boolean oldValue, 
-            boolean newValue){
-            // If the PropertyChangeSupport has been initialized
-        if (changeSupport != null)  
-            changeSupport.firePropertyChange(propertyName, oldValue, newValue);
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name and 
-     * new value. This method is for {@code boolean} properties and the old 
-     * value is assumed to be the inverse of the new value.
-     * @param propertyName The name of the property.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, boolean newValue){
-        firePropertyChange(propertyName, !newValue, newValue);
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for character properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, char oldValue, 
-            char newValue){
-        firePropertyChange(propertyName,Character.valueOf(oldValue),
-                Character.valueOf(newValue));
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for integer properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, int oldValue, 
-            int newValue){
-            // If the PropertyChangeSupport has been initialized
-        if (changeSupport != null)  
-            changeSupport.firePropertyChange(propertyName, oldValue, newValue);
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for byte properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, byte oldValue, 
-            byte newValue){
-        firePropertyChange(propertyName,Byte.valueOf(oldValue),
-                Byte.valueOf(newValue));
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for short properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, short oldValue, 
-            short newValue){
-        firePropertyChange(propertyName,Short.valueOf(oldValue),
-                Short.valueOf(newValue));
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for long properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, long oldValue, 
-            long newValue){
-        firePropertyChange(propertyName,Long.valueOf(oldValue),
-                Long.valueOf(newValue));
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for float properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, float oldValue, 
-            float newValue){
-        firePropertyChange(propertyName,Float.valueOf(oldValue),
-                Float.valueOf(newValue));
-    }
-    /**
-     * This fires a {@code PropertyChangeEvent} with the given property name, 
-     * old value, and new value. This method is for double properties.
-     * @param propertyName The name of the property.
-     * @param oldValue The old value.
-     * @param newValue The new value.
-     */
-    protected void firePropertyChange(String propertyName, double oldValue, 
-            double newValue){
-        firePropertyChange(propertyName,Double.valueOf(oldValue),
-                Double.valueOf(newValue));
     }
     
     
