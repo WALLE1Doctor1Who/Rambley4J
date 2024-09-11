3640d07cbbd5a5a69c0afe6e64a0da41d17ea318
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-09-11 01:19:13 -0500

    Renamed RambleyTestViewer -> RambleyPainterTester
    I've renamed RambleyTestViewer to RambleyPainterTester.

--- src/raccoon/RambleyPainterTester.java
+++ src/raccoon/RambleyPainterTester.java
@@ -23,7 +23,7 @@ import javax.swing.*;
  *
  * @author Milo Steier
  */
-public class RambleyTestViewer extends javax.swing.JFrame {
+public class RambleyPainterTester extends javax.swing.JFrame {
     /**
      * This is the name of the preference node used to store the settings for 
      * this program.
@@ -89,7 +89,7 @@ public class RambleyTestViewer extends javax.swing.JFrame {
     /**
      * Creates new form RambleyViewer
      */
-    public RambleyTestViewer() {
+    public RambleyPainterTester() {
         rambley = new RambleyTestIcon();
         debugIcon = new DebuggingIcon((Icon)rambley,false);
         initComponents();
@@ -1151,21 +1151,23 @@ public class RambleyTestViewer extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RambleyTestViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RambleyPainterTester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RambleyTestViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RambleyPainterTester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RambleyTestViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RambleyPainterTester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RambleyTestViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(RambleyPainterTester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new RambleyTestViewer().setVisible(true);
+                new RambleyPainterTester().setVisible(true);
             }
         });
     }
