11804cd4eb00ff88e644bf5c3f80c31b98497cf5
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-09-24 19:07:50 -0500

    Create BackgroundPainter.java
    Added an unfinished background painter.

---
+++ src/raccoon/BackgroundPainter.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package raccoon;
+
+import java.awt.*;
+import swing.ListenedPainter;
+
+/**
+ *
+ * @author Milo Steier
+ */
+public class BackgroundPainter extends ListenedPainter<Component>{
+    
+    
+    
+    /**
+     * This is the default width and height of the background polka dots.
+     */
+    protected static final double DEFAULT_BACKGROUND_DOT_SIZE = 8.0;
+    /**
+     * This is the default diagonal spacing between the centers of the 
+     * background polka dots. Refer to the documentation for the {@link 
+     * #getBackgroundDotSpacing getBackgroundDotSpacing} method for a more in
+     * depth description on the background polka dot spacing.
+     */
+    protected static final double DEFAULT_BACKGROUND_DOT_SPACING = 12.0;
+    
+    
+    
+    /**
+     * This identifies that a change has been made to the width and height of 
+     * the background polka dots.
+     */
+    public static final String BACKGROUND_DOT_SIZE_PROPERTY_CHANGED = 
+            "BackgroundDotSizePropertyChanged"; 
+    /**
+     * This identifies that a change has been made to the spacing of the 
+     * background polka dots.
+     */
+    public static final String BACKGROUND_DOT_SPACING_PROPERTY_CHANGED = 
+            "BackgroundDotSpacingPropertyChanged"; 
+    
+    
+    
+    /**
+     * The width and height of the background polka dots.
+     */
+    private double dotSize;
+    /**
+     * This is the diagonal spacing between the centers of the background polka 
+     * dots. That is to say, the center of each background polka dot is {@code 
+     * dotSpacing} pixels to the left and {@code dotSpacing} pixels below the 
+     * center of another background polka dot.
+     */
+    private double dotSpacing;
+    
+    private Color color;
+    private Color dotColor;
+    private Color gradientColor;
+    private Color gradientColor2;
+
+    @Override
+    public void paint(Graphics2D g, Component object, int width, int height) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
