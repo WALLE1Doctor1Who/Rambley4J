70aa1efa336c522a9d3aeb14ceb66f17256ba318
WALLE1Doctor1Who <msteier2.0@gmail.com>
2024-09-26 07:17:12 -0500

    Added code for scaling the pixel grid spacing
    I've added code to scale the pixel grid effect's line spacing to PixelGridPainter.

--- src/raccoon/PixelGridPainter.java
+++ src/raccoon/PixelGridPainter.java
@@ -43,6 +43,11 @@ public class PixelGridPainter extends ListenedPainter<Component>{
     public static final String LINE_THICKNESS_PROPERTY_CHANGED = 
             "PixelGridThicknessPropertyChanged";
     /**
+     * This is the width and height at which the pixel grid is drawn at 
+     * internally.
+     */
+    private static final double INTERNAL_SIZE = 256;
+    /**
      * This is the spacing between the lines in the pixel grid. For the vertical 
      * lines, this is the horizontal spacing. For the horizontal lines, this is 
      * the vertical spacing.
@@ -143,16 +148,34 @@ public class PixelGridPainter extends ListenedPainter<Component>{
         return this;
     }
     /**
+     * 
+     * @param size
+     * @return 
+     */
+    protected double getScale(double size){
+        return size / INTERNAL_SIZE;
+    }
+    /**
+     * 
+     * @param width
+     * @param height
+     * @return 
+     */
+    protected double getScaledLineSpacing(double width, double height){
+        return getLineSpacing() * getScale(Math.min(width, height));
+    }
+    /**
      * This is used to calculate the offset for the pixel grid effect using the 
      * given size value.
+     * @param spacing
      * @param size The value to use to get the offset.
      * @return The offset for the pixel grid effect.
      * @see getPixelGridOffsetX
      * @see getPixelGridOffsetY
      * @see getLineSpacing
      */
-    private double getPixelGridOffset(double size){
-        return ((size-1)%getLineSpacing())/2.0;
+    private double getPixelGridOffset(double spacing, double size){
+        return ((size-1)%spacing)/2.0;
     }
     /**
      * This returns the x offset to use for the horizontal lines of the pixel 
@@ -161,6 +184,7 @@ public class PixelGridPainter extends ListenedPainter<Component>{
      * @implSpec The default implementation is equivalent to {@code ((width-1) 
      * %} {@link getLineSpacing() getLineSpacing()}{@code )/2.0}.
      * 
+     * @param spacing
      * @param width The width of the area to fill with the pixel grid effect.
      * @return The offset for the x-coordinate of the pixel grid effect.
      * @see #getPixelGridOffsetY
@@ -169,8 +193,8 @@ public class PixelGridPainter extends ListenedPainter<Component>{
      * @see getLineSpacing
      * @see setLineSpacing
      */
-    protected double getPixelGridOffsetX(double width){
-        return getPixelGridOffset(width);
+    protected double getPixelGridOffsetX(double spacing, double width){
+        return getPixelGridOffset(spacing, width);
     }
     /**
      * This returns the y offset to use for the vertical lines of the pixel grid 
@@ -179,6 +203,7 @@ public class PixelGridPainter extends ListenedPainter<Component>{
      * @implSpec The default implementation is equivalent to {@code ((height-1) 
      * %} {@link getLineSpacing() getLineSpacing()}{@code )/2.0}.
      * 
+     * @param spacing
      * @param height The height of the area to fill with the pixel grid effect.
      * @return The offset for the y-coordinate of the pixel grid effect.
      * @see #getPixelGridOffsetY
@@ -187,8 +212,8 @@ public class PixelGridPainter extends ListenedPainter<Component>{
      * @see getLineSpacing
      * @see setLineSpacing
      */
-    protected double getPixelGridOffsetY(double height){
-        return getPixelGridOffset(height);
+    protected double getPixelGridOffsetY(double spacing, double height){
+        return getPixelGridOffset(spacing, height);
     }
     /**
      * This returns the Path2D object used to render the pixel grid effect. The 
@@ -222,16 +247,18 @@ public class PixelGridPainter extends ListenedPainter<Component>{
         double x2 = x+w;
             // Get the maximum y-coordinate for the pixel grid
         double y2 = y+h;
+            // Get the spacing for the lines.
+        double spacing = getScaledLineSpacing(w,h);
             // Go through and generate the vertical lines, starting at the 
             // offset for the y-coordinate of the pixel grid and spacing them 
             // out by the pixel grid spacing
-        for (double y1 = getPixelGridOffsetY(h); y1 <= h; y1+=getLineSpacing()){
+        for (double y1 = getPixelGridOffsetY(spacing,h); y1 <= h; y1+=spacing){
             path.moveTo(x, y1+y);
             path.lineTo(x2, y1+y);
         }   // Go through and generate the horizontal lines, starting at the 
             // offset for the x-coordinate of the pixel grid and spacing them 
             // out by the pixel grid spacing
-        for (double x1 = getPixelGridOffsetX(w); x1 <= w; x1+=getLineSpacing()){
+        for (double x1 = getPixelGridOffsetX(spacing,w); x1 <= w; x1+=spacing){
             path.moveTo(x1+x, y);
             path.lineTo(x1+x, y2);
         }
